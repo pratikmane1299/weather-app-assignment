9fde58f28206789e4e0b92b20548ade34bf7e875
Pratik Mane <pratik.mane1299@gmail.com>
2024-04-28 20:31:50 +0530

    improve user experience when switching units and/or duration
    use useOptimistic and useTransition to improve ux.

--- src/app/components/tabs.tsx
+++ src/app/components/tabs.tsx
@@ -1,10 +1,14 @@
 'use client';
 import { cn } from "@/utils";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useOptimistic, useTransition } from "react";
 
 const durations = [{ label: "Today", value: 'today' }, { label: "Week", value: 'week' }]
 
 export default function DurationTabs({ active }: { active: string }) {
+
+  const [optimisticDuration, setDuration] = useOptimistic(active);
+  const [_, setTransition] = useTransition();
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -19,14 +23,18 @@ export default function DurationTabs({ active }: { active: string }) {
 
     const params = current.toString();
     const query = params ? `?${params}` : '';
-    router.push(query);
+
+    setTransition(() => {
+      setDuration(duration);
+      router.push(query);
+    });
   }
 
   return (
     <ul className="flex items-center space-x-4">
       {durations.map((duration) => (
         <li key={duration.value}>
-          <span className={cn("cursor-pointer text-base py-1.5 ", active === duration.value ? "font-semibold text-gray-900 border-b-2 border-gray-900" : "text-gray-400")} onClick={() => changeDuration(duration.value)}>{duration.label}</span>
+          <span className={cn("cursor-pointer text-base py-1.5 ", optimisticDuration === duration.value ? "font-semibold text-gray-900 border-b-2 border-gray-900" : "text-gray-400")} onClick={() => changeDuration(duration.value)}>{duration.label}</span>
         </li>
       ))}
     </ul>
--- src/app/components/unit-switcher.tsx
+++ src/app/components/unit-switcher.tsx
@@ -2,11 +2,14 @@
 
 import { cn } from "@/utils";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useOptimistic, useTransition } from "react";
 
 const units = [{ label: "C", value: 'celcius' }, { label: "F", value: 'farhenheit' }];
 export default function UnitSwitcher({ currentUnit }: { currentUnit: string }) {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [optimisticUnit, setUnit] = useOptimistic(currentUnit);
+  const [_, setTransition] = useTransition();
 
   function changeUnit(unit: string) {
     const current = new URLSearchParams(Array.from(searchParams.entries()));
@@ -19,14 +22,18 @@ export default function UnitSwitcher({ currentUnit }: { currentUnit: string }) {
 
     const params = current.toString();
     const query = params ? `?${params}` : '';
-    router.push(query);
+
+    setTransition(() => {
+      setUnit(unit);
+      router.push(query);
+    });
   }
 
   return (
     <ul className="flex items-center space-x-3">
       {units.map((unit) => (
         <li key={unit.value}>
-          <span className={cn("cursor-pointer p-2 w-7 h-7 flex justify-center items-center rounded-full  ", currentUnit === unit.value ? "bg-gray-900 text-white" : "bg-white text-gray-900")} onClick={() => changeUnit(unit.value)}>
+          <span className={cn("cursor-pointer p-2 w-7 h-7 flex justify-center items-center rounded-full  ", optimisticUnit === unit.value ? "bg-gray-900 text-white" : "bg-white text-gray-900")} onClick={() => changeUnit(unit.value)}>
             <sup>.</sup>{unit.label}
           </span>
         </li>
